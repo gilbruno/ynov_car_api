d4854c8c431d89342aa2fdd075be0c5ce02b0462
Gilles Bruno <gilles_bruno@gmx.fr>
2023-11-02 16:44:44 +0100

    Move ValidationPipe in module

--- src/main.ts
+++ src/main.ts
@@ -14,11 +14,11 @@ async function bootstrap() {
       }
     )
   )
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true
-    })
-  )
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     whitelist: true
+  //   })
+  // )
   await app.listen(3000);
 
   await displayRoutes(app)
