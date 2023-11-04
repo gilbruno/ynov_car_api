d944535ab8fca41acb8138dc1777d9dcc0932fa2
Gilles Bruno <gilles_bruno@gmx.fr>
2023-11-04 18:35:25 +0100

    Create report creation route

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -8,4 +8,32 @@ export class Report {
 
     @Column()
     price: number
+
+    /**
+     * Company that make the vehicle
+     */
+    @Column()
+    make: string
+
+    @Column()
+    model: string
+
+    @Column()
+    year: number
+
+    /**
+     * Longitude where the vehicle is sold
+     */
+    @Column()
+    lng: number
+
+    /**
+     * Latitude where the vehicle is sold
+     */
+    @Column()
+    lat: number
+    
+    @Column()
+    mileage: number
+
 }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { CreateReportDto } from './dtos/create-report.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Report } from './report.entity';
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+
+    constructor(@InjectRepository(Report) private repo: Repository<Report>) {
+
+    }
+
+    create(reportDto: CreateReportDto) {
+        const report = this.repo.create(reportDto)
+        this.repo.save
+    }
+}
