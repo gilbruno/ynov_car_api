fb9a488317c938a77657177ae0651e66e39be366
Gilles Bruno <gilles_bruno@gmx.fr>
2023-11-07 13:06:33 +0100

    Handlin association when report is created

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from '../guards/auth.guard';
+import { CurrentUser } from '../users/decorators/current-user.decorator';
+import { User } from '../users/user.entity';
 
 @Controller('reports')
 export class ReportsController {
@@ -11,7 +13,7 @@ export class ReportsController {
     }
     @Post()
     @UseGuards(AuthGuard)
-    createReport(@Body() body:CreateReportDto) {
-        this.reportsService.create(body)
+    createReport(@Body() body:CreateReportDto, @CurrentUser() user: User) {
+        this.reportsService.create(body,user)
     }
 }
--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -3,6 +3,7 @@ import { CreateReportDto } from './dtos/create-report.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Report } from './report.entity';
+import { User } from '../users/user.entity';
 
 @Injectable()
 export class ReportsService {
@@ -11,8 +12,9 @@ export class ReportsService {
 
     }
 
-    create(reportDto: CreateReportDto) {
+    create(reportDto: CreateReportDto, user: User) {
         const report = this.repo.create(reportDto)
+        report.user = user
         this.repo.save
     }
 }
