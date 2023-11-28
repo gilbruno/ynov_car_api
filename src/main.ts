717b231ffd23b71a7e204995a3b2a8c83d73ff3c
Gilles Bruno <gilles_bruno@gmx.fr>
2023-11-28 12:34:24 +0100

    Preparation to deploy on Heroku

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ const cookieSession = require('cookie-session')
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 
   await displayRoutes(app)
 }
